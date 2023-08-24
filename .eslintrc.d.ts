eda59093b44d98a39567bb9d86126544ec8a3a5b
Adrenaline1477 <nikita-bull2@mail.ru>
2023-08-24 18:23:02 +0300

    Добавил модули

---
+++ .eslintrc.d.ts
@@ -0,0 +1,14 @@
+declare module '*.js' {
+    const value: any;
+    export = value;
+}
+
+declare module '*.ts' {
+    const value: any;
+    export = value;
+}
+
+declare module '*.tsx' {
+    const value: any;
+    export = value;
+}
